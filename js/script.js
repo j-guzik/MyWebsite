535ca73d2ccc334d78a247d2f556c5f3ff21ff61
j-guzik <justyna.b.guzik@gmail.com>
2021-02-21 23:51:39 +0100

    mobile view

--- js/script.js
+++ js/script.js
@@ -1,4 +1,29 @@
-window.addEventListener('scroll', function(){
-    var header = document.querySelector('header');
+var header = document.querySelector('header');
+
+window.addEventListener('scroll', function () {
     header.classList.toggle('sticky', window.scrollY > 0)
 })
+
+const burger = document.querySelector(".menu-circle");
+burger.addEventListener("click", function () {
+    burger.classList.toggle("active");
+    // document.body.style.overflowY("hidden");
+
+
+
+    //jak zrobic zeby kazde SelectorAll petla?
+
+    var textWrapper = document.querySelector('.letters');
+    textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+    anime.timeline(1)
+        .add({
+            targets: '.letters .letter',
+            opacity: [0, 1],
+            easing: "easeInOutQuad",
+            duration: 2250,
+            delay: (el, i) => 150 * (i + 1)
+        });
+})
+
+// jesli szerokosc okna mniejsza niz 1000 dodaj do header sticky
