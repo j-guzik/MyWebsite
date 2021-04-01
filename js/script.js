3f9c6605ecbc66b4d75ccf80a688ab0556a986ca
j-guzik <justyna.b.guzik@gmail.com>
2021-04-01 23:18:10 +0200

    new movie

--- js/script.js
+++ js/script.js
@@ -1,171 +1,7 @@
-// change menu appearance
-
-var header = document.querySelector('header');
-
-    // if ($(window).width() >= 984) {
-        window.addEventListener('scroll', function () {
-            header.classList.toggle('sticky', window.scrollY > 0)
-        })
-    // }
-
-
-// function checkWidth(init)
-// {
-//     /*If browser resized, check width again */
-//     if ($(window).width() < 984) {
-//         $('header').addClass('sticky');
-//     }
-//     else {
-//         if (!init) {
-//             $('header').removeClass('sticky');
-//         }
-        
-//     }
-// }
-
-// $(document).ready(function() {
-//     checkWidth(true);
-
-//     $(window).resize(function() {
-//         checkWidth(false);
-//     });
-// });
-
-
-
-// mobile menu
-
-$(".menu-circle").on("click", function () {
-    $(this).toggleClass("active");
-    $('body , html').toggleClass("stop-scroll");
-})
-
-// burger.addEventListener("click", function () {
-//     burger.classList.toggle("active");
-//     // document.body.style.overflow("hidden");
-//     document.body.classList.toggle('lock-scroll');
-
-
-
-
-// $(window).resize(function () {//begin resize event
-
-
-//     //get the window width
-//     var winWidth = $(window).width();
-
-//     //set the maxWidth
-//     var maxWidth = 1000;
-
-
-//     //if the window width is less than maxWidth pixels
-//     if (winWidth < maxWidth) {//begin if then
-
-//         //add the class hidden to .jobFilter         
-//         $(header).addClass("sticky");
-
-//     }
-//     else {
-
-//         //remove the class hidden                 
-//         $(header).removeClass("sticky");
-
-
-//     }//end if then else
-
-
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var textWrapper = document.querySelector('.letters');
-// textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-
-// anime.timeline(1)
-//     .add({
-//         targets: '.letters .letter',
-//         opacity: [0, 1],
-//         easing: "easeInOutQuad",
-//         duration: 2250,
-//         delay: (el, i) => 150 * (i + 1)
-//     });
-
-
-// jesli szerokosc okna mniejsza niz 1000 dodaj do header sticky
-
-
-
-
-
-
-// loader:
-// setTimeout(function () { $(".loader").fadeOut("slow"); }, 5000);
-
-
-
-//modal:
-$('.button-video, i.close').on('click', function () {
-    $(".modal-wrap").toggleClass("active");
-    $('body , html').toggleClass("stop-scroll");
-})
-
-var video = document.querySelector('.video');
-function pauseVideo() {
-    video.pause();
-}
-
-$('i.close').on('click', pauseVideo); // PAUZA DZIALA TYLKO JAK JEST JEDNE ROZWIAZ TO IFEM CZY CZYMS< DODATKOWYMI KLASAMi
-
-
-
-
-
-
-
-$(document).on('scroll', function () {
-
-    const windowHeight = $(window).height();
-    const scrollValue = $(this).scrollTop();
-    // console.log(scrollValue);
-
-
-    const $technologies = $('.technologies');
-    const $infoabout = $('.info-about');
-    const $about = $('.about');
-    const aboutFromTop = $about.offset().top;
-    const aboutHeight = $about.outerHeight(true);
-
-    if (scrollValue > aboutFromTop + aboutHeight - windowHeight) {
-        $infoabout.addClass('active');
-        $technologies.addClass('active');
-    }
-
-    if (scrollValue < 100) {
-        $infoabout.removeClass('active');
-    }
-
-})
-
-
-
-
-// typing:
+// typing
 
 const box = document.querySelector('.typing');
-const text = ["Hi! I'm Justyna!", "I'm Frontend Developer.", "Below you will find some information about me and my projects.", " "];
+const text = ["Hi! I'm Justyna!", "I'm a Computer Science student.", "Below you will find some information about me and my projects.", " "];
 let wordIndex = 0;
 let oldTime = 0;
 const speed = 100;
@@ -181,7 +17,6 @@ const typing = (newTime) => {
             }
 
             return setTimeout(() => {
-            // box.textContent = " ";
             textIndex++;
             wordIndex = 0;
             requestAnimationFrame(typing);
@@ -203,3 +38,90 @@ const typing = (newTime) => {
 }
 
 typing();
+
+
+// change menu appearance
+
+window.addEventListener('scroll', function () {
+    const header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 0)
+})
+
+
+// stop page scrolling
+
+function stopScroll(){
+    $('body , html').toggleClass("stop-scroll");
+}
+
+
+// mobile menu
+
+function toggleMenu(){
+    $(".menu-circle").toggleClass("active");
+}
+
+function menu(){
+    toggleMenu();
+    $('body , html').removeClass("stop-scroll");
+}
+
+function circleMenu(){
+    toggleMenu();
+    stopScroll();
+}
+
+
+// animation when hovering over the menu
+
+function bubbles() {
+    $.each($(".particletext.bubbles"), function(){
+       var bubblecount = ($(this).width()/60)*10;
+       for(var i = 0; i <= bubblecount; i++) {
+        const colors = ["#9745FC", "#54E6F1"];
+          var size = ($.rnd(40,80)/9);
+          var  backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+          $(this).append('<span class="particle" style="top:' + $.rnd(0,100) + '%; left:' + $.rnd(0,97) + '%;width:' + size + 'px; height:' + size + 'px;animation-delay: ' + ($.rnd(0,20)/10) + 's;background-color:' + backgroundColor +';"></span>');
+       }
+    });
+ }
+
+ jQuery.rnd = function(m,n) {
+    m = parseInt(m);
+    n = parseInt(n);
+    return Math.floor( Math.random() * (n - m + 1) ) + m;
+}
+
+bubbles();
+
+
+// scrolling the page to the appropriate section
+
+$('li a').on('click', function () {
+    const goToSection = '#' + $(this).attr('data-section');
+    $('body, html').animate({
+        scrollTop: $(goToSection).offset().top
+    })
+})
+
+//modal
+
+$('.button-video-museum, i.closeMuseum').on('click', function () {
+    $("#museumManagement").toggleClass("active");
+    stopScroll();
+});
+
+$('.button-video-python, i.closePython').on('click', function () {
+    $("#pythonGame").toggleClass("active");
+    stopScroll();
+});
+
+$('i.closeMuseum').on('click', function () {
+    const videoMuseumManagement = document.querySelector('#videoMuseumManagement');
+    videoMuseumManagement.pause();
+});
+
+$('i.closePython').on('click', function () {
+    const videoPythonGame = document.querySelector('#videoPythonGame');
+    videoPythonGame.pause();
+});
